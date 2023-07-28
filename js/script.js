3dbcf7a794333170bd3bb0de09dd919fb502969b
RAIKIRI-SYS <73961646+RAIKIRI-SYS@users.noreply.github.com>
2023-07-28 15:58:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ new Swiper(' .main__slider', {
+    slidesPerView:3,
+    breakpoints:{
+      320: {
+         slidesPerView: 1,
+      },
+      600: {
+         slidesPerView: 2,
+      },
+      970: {
+         slidesPerView: 3,
+      }
+    },
+ });
+
+ 
+ 
+
